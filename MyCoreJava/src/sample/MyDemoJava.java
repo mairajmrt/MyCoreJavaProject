04af4daf1189061f44988cf9e0bf6857c4cd7982
mali204 <mali204@DUC-LAn20q3gaZ3.dedalus.lan>
2025-06-06 18:07:46 +0530

    By Mairaj Ali some dots are added.....

--- MyCoreJava/src/sample/MyDemoJava.java
+++ MyCoreJava/src/sample/MyDemoJava.java
@@ -3,7 +3,7 @@ package sample;
 public class MyDemoJava {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Demo Java");
+	    System.out.println("Hello Demo Java...........");
 
 	}
 
